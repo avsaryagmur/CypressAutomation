d195ed3604f8f2fa470f31dab2296db26ec2cf96
yagmuravsar <yagmur.avsar@deliveryhero.com>
2023-07-19 12:22:41 +0200

    POM Pattern

---
+++ cypress/pageObjects/LoginPage.js
@@ -0,0 +1,12 @@
+export class Login {
+    constructor() {
+      this.usernameInput = "input[placeholder='Username']";
+      this.passwordInput = "input[placeholder='Password']";
+      this.loginBtn = "button[type='submit']";
+      this.verfiyLogin = '.oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module';
+    }
+  
+  }
+
+
+  
